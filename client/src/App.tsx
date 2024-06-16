9cbe0fd71574a365edaa4135d8a9a851fbdfecf2
Darren Hoang <darrenhoang12@gmail.com>
2024-06-16 11:22:27 -0700

    Added ability to change select shelf names

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,36 @@ import { useState } from "react";
 function App() {
   const [warehouseName, setWarehouseName] = useState("");
   const [zoneShelves, setZoneShelves] = useState(
-    Array.from({ length: 12 }, () => 1)
+    Array.from({ length: 12 }, (_, index) => ({
+      shelves: 1,
+      shelfNames: [""],
+    }))
   );
 
   function handleZoneShelvesChange(index: number, value: number) {
     const updatedZones = [...zoneShelves];
-    updatedZones[index] = value;
+    if (value > updatedZones[index].shelfNames.length) {
+      updatedZones[index].shelfNames = updatedZones[index].shelfNames.concat(
+        new Array(value - updatedZones[index].shelfNames.length).fill("")
+      );
+    } else {
+      updatedZones[index].shelfNames = updatedZones[index].shelfNames.slice(
+        0,
+        value
+      );
+    }
+    updatedZones[index].shelves = value;
+    setZoneShelves(updatedZones);
+    console.log(zoneShelves);
+  }
+
+  function handleShelfNamesChange(
+    indexZone: number,
+    indexShelf: number,
+    value: string
+  ) {
+    const updatedZones = [...zoneShelves];
+    updatedZones[indexZone].shelfNames[indexShelf] = value;
     setZoneShelves(updatedZones);
     console.log(zoneShelves);
   }
@@ -19,8 +43,8 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Factory X Factory Creator</h1>
+    <div className="app">
+      <h1>Factory X Warehouse Creator</h1>
       <form>
         <label htmlFor="warehouse_name">Warehouse Name:</label>
         <input
@@ -31,23 +55,37 @@ function App() {
         ></input>
         <h4>Select up to a maximum of 10 shelves per zone</h4>
         <div className="zones">
-          {zoneShelves.map((value: number, index: number) => (
+          {zoneShelves.map((_, index: number) => (
             <div className="zone" key={index}>
-              <label htmlFor={`zone${index}`}>Zone {index + 1}:</label>
-              <select
-                className="selectBox"
-                id={`zone${index}`}
-                value={value}
-                onChange={(e) => {
-                  handleZoneShelvesChange(index, parseInt(e.target.value));
-                }}
-              >
-                {Array.from({ length: 10 }, (_, i) => (
-                  <option key={i} value={i + 1}>
-                    {i + 1}
-                  </option>
+              <div className="zoneSelect">
+                <label htmlFor={`zone${index}`}>Zone {index + 1}: </label>
+                <select
+                  className="selectBox"
+                  id={`zone${index}`}
+                  onChange={(e) => {
+                    handleZoneShelvesChange(index, parseInt(e.target.value));
+                  }}
+                >
+                  {Array.from({ length: 10 }, (_, i) => (
+                    <option key={i} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="shelfInputs">
+                {Array.from({ length: zoneShelves[index].shelves }, (_, i) => (
+                  <div className="shelfInput" key={i}>
+                    <label>Name of Shelf {i + 1}: </label>
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        handleShelfNamesChange(index, i, e.target.value);
+                      }}
+                    ></input>
+                  </div>
                 ))}
-              </select>
+              </div>
             </div>
           ))}
         </div>
