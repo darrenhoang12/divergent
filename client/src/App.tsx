122ffe86d3145fc98488e2198fb83ba7dfc4f272
Darren Hoang <darrenhoang12@gmail.com>
2024-06-15 18:09:02 -0700

    First commit - UI complete

---
+++ client/src/App.tsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [warehouseName, setWarehouseName] = useState("");
+  const [zoneShelves, setZoneShelves] = useState(
+    Array.from({ length: 12 }, () => 1)
+  );
+
+  function handleZoneShelvesChange(index: number, value: number) {
+    const updatedZones = [...zoneShelves];
+    updatedZones[index] = value;
+    setZoneShelves(updatedZones);
+    console.log(zoneShelves);
+  }
+
+  function createFactory() {
+    console.log(zoneShelves);
+  }
+
+  return (
+    <div>
+      <h1>Factory X Factory Creator</h1>
+      <form>
+        <label htmlFor="warehouse_name">Warehouse Name:</label>
+        <input
+          type="text"
+          id="warehouse_name"
+          value={warehouseName}
+          onChange={(e) => setWarehouseName(e.target.value)}
+        ></input>
+        <h4>Select up to a maximum of 10 shelves per zone</h4>
+        <div className="zones">
+          {zoneShelves.map((value: number, index: number) => (
+            <div className="zone">
+              <label htmlFor={`zone${index}`}>Zone {index + 1}:</label>
+              <select
+                className="selectBox"
+                id={`zone${index}`}
+                value={value}
+                onChange={(e) => {
+                  handleZoneShelvesChange(index, parseInt(e.target.value));
+                }}
+              >
+                {Array.from({ length: 10 }, (_, i) => (
+                  <option value={i + 1}>{i + 1}</option>
+                ))}
+              </select>
+            </div>
+          ))}
+        </div>
+        <button className="submit" onClick={createFactory}>
+          Create Factory
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
